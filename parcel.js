250011ef792a7356d5d58bb0816a568ff99c8ce3
Mayank Sharma <mayank.sharma@easygov.co.in>
2023-01-29 14:52:02 +0530

    Day 2 learning

---
+++ parcel.js
@@ -0,0 +1,21 @@
+/*  
+**Parcel**  npx parcel index.html
+ Bundling, 
+ minifiy code, 
+ File Watcher algorithm, 
+ hot module loading, 
+ cleaning of code -> remove console.log
+ dev and prod build manage
+ minify node modules
+ image optimization
+ Caching while development
+ Compresssion
+ Renaming variables
+ Compataible with older versions of browser
+ Add Pollyfills
+ HTTPS on dev
+ Manage port number
+ Consistent hashing algorithm
+ Zero Configuration
+ 
+*/
